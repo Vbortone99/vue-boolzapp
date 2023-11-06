230d8bdbbce3a7d1939231c1e78910e90c52fd7f
Vbortone99 <bortonevincenzo99@gmail.com>
2023-11-06 16:14:33 +0100

    Vue data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
+import {contactList} from './data.js' 
 const {createApp} = Vue;
     createApp({
         data(){
-
+            return {
+                contacts: contactList,
+                activeContactIndex: 0,
+                searchText:'',
+                message:'',
+            }
         },
         methods:{
 
